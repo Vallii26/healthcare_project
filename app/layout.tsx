f7c20512f5a085c9c521e0e467ea6fc02b1f3c31
Mikel Valladares <mikelvalladaresbarrera@gmail.com>
2024-07-21 03:22:44 +0200

    change title

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,7 +12,7 @@ const fontSans = Plus_Jakarta_Sans({
 });
 
 export const metadata: Metadata = {
-  title: "Careplus",
+  title: "CarePulse",
   description: "A healthcare management system",
 };
 
